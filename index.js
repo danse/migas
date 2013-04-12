7ca338d823dc500409bbd48cfd3a885d2735d28b
danse <f.occhipinti@gmail.com>
2013-04-12 23:03:56 +0200

    why not local storage?

--- index.js
+++ index.js
@@ -3,13 +3,38 @@ setInterval(function() {
     var size = $('input').attr('size');
     $('input').attr('size', Number(size)+1);
 }, minute);
+var wrapper = {
+    records: [],
+    handler: function(){},
+    add: function(record) {
+        this.handler(record);
+        this.records.push(record);
+        localStorage['records'] = JSON.stringify(this.records);
+    },
+    reload: function() {
+        try {
+            this.records = JSON.parse(localStorage.getItem('records'));
+        } catch(e) {
+            console.log('error parsing '+localStorage['records']);
+        }
+        if(this.records) {
+            this.records.map(this.handler);
+        } else {
+            this.records = [];
+        }
+    }
+};
+var handler = function(record) {
+    $('body').append('<br>', record);
+};
+wrapper.handler = handler;
 onload = function() {
     $('input').keyup(function(e) {
         if(e.which===13) {
-            var $i = $('input'),
-                $b = $('body');
-            $b.append('<br>', [$i.attr('size'), ' '+$i.prop('value')]);
+            var $i = $('input');
+            wrapper.add([$i.attr('size'), ' '+$i.prop('value')]);
             $i.prop('value', '').attr('size', 0);
         }
     });
+    wrapper.reload();
 };
