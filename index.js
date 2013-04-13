51ab20a2ef473aa4f7bd33ce925fea8b5f69d472
danse <f.occhipinti@gmail.com>
2013-04-13 17:51:41 +0200

    mainly debugged. the wrapper is not a wrapper anymore, it is not a wrap, is a nonsense object, i could also call it `burrito`

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 var minute = 60000;
+
 function set(n) {
     $('input')
         .prop('value', '')
         .attr('size', n+1) // size 0 won't work
         .attr('maxlength', n);
 }
+
 function reset() { set(0); }
+
 setInterval(function() {
     var n = Number($('input').attr('size')) + 1;
     set(n);
 }, minute);
-var wrapper = {
-    records: [],
-    handler: function(){},
+
+var burrito = {
+    append: function(record){
+        $('.report').append('<br>', record);
+    },
     add: function(record) {
-        this.handler(record);
+        this.append(record);
         this.records.push(record);
         this.save();
     },
@@ -26,31 +31,26 @@ var wrapper = {
             this.records = JSON.parse(localStorage.getItem('records'));
         } catch(e) {
             console.log('error parsing '+localStorage['records']);
-        }
-        if(this.records) {
-            this.records.map(this.handler);
-        } else {
             this.records = [];
         }
+        this.records.map(this.append);
     },
     clear: function() {
         this.records = [];
         this.save();
+        $('.report').empty();
     }
 };
-var handler = function(record) {
-    $('body').append('<br>', record);
-};
-wrapper.handler = handler;
+
 onload = function() {
     $('input').keyup(function(e) {
         if(e.which===13) {
             var $i = $('input');
-            wrapper.add([$i.attr('size'), ' '+$i.prop('value')]);
+            burrito.add([$i.attr('size'), ' '+$i.prop('value')]);
             reset();
         }
     });
-    wrapper.load();
-    $('button').click(wrapper.clear());
+    $('button').click(burrito.clear);
+    burrito.load();
     reset();
 };
